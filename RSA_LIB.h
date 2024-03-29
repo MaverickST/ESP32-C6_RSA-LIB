deaa5d460a0982969846ac7e5e45b78b199117bf
MaverickST <maverick.sossa@udea.edu.co>
2024-03-28 20:21:26 -0500

    It is done the library, but it is missing a revision

--- RSA_LIB.h
+++ RSA_LIB.h
@@ -31,13 +31,215 @@
  *    b. Register masks to each bit and bitfield in the register
  *    c. Bitifield constant values masks
  * 2. Module structure definition
- *    a. Data type
- *    b. Module masks
- *    c. Structure access macro
- * 3. Register access macros
+ *    a. Module data type using a structure
+ *    c. Structure access macro to each module of the type selected
+ * 3. Register access macros definition
  *    a. Whole register access macro
  *    b. Bitfields access macro
  * 
  * 
- * 
  */
+#ifndef RSA_LIB_H_
+#define RSA_LIB_H_
+
+#include <stdint.h>
+
+/*******************************************************************/
+/****************Start Bits Fields Type Definition******************/
+/*******************************************************************/
+
+typedef union{ // Represents M’. (R/W)
+	uint32_t WORD;
+	struct{
+		uint32_t MPRIME	:32;
+	}BITS;
+}__rsa_mprime_t; //  RSA_M_PRIME_REG (0x0800)
+
+typedef union{ // Configures the RSA length. (R/W)
+	uint32_t WORD;
+	struct{
+		uint32_t MODE	:7;
+		uint32_t		:25;
+	}BITS;
+}__rsa_mode_t; // RSA_MODE_REG (0x0804)
+
+typedef union{ // Configures whether or not to starts the modular exponentiation.
+	uint32_t WORD;
+	struct{
+		uint32_t START_MODEXP	:1;
+		uint32_t				:31;
+	}BITS;
+}__rsa_modexp_t; // RSA_SET_START_MODEXP_REG (0x080C)
+
+typedef union{ // Configures whether or not to start the modular multiplication.
+	uint32_t WORD;
+	struct{
+		uint32_t START_MODMULT	:1;
+		uint32_t				:31;
+	}BITS;
+}__rsa_modmult_t; // RSA_SET_START_MODMULT_REG (0x0810)
+
+typedef union{ // Configures whether or not to start the multiplication.
+	uint32_t WORD;
+	struct{
+		uint32_t START_MULT	:1;
+		uint32_t			:31;
+	}BITS;
+}__rsa_mult_t; // RSA_SET_START_MULT_REG (0x0814)
+
+typedef union{ // Represents the RSA status.
+	uint32_t WORD;
+	struct{
+		uint32_t Q_IDLE	:1;
+		uint32_t		:31;
+	}BITS;
+}__rsa_status_t; // RSA_QUERY_IDLE_REG (0x0818)
+
+typedef union{ // Configures the constant_time option.
+	uint32_t WORD;
+	struct{
+		uint32_t kTIME	:1;
+		uint32_t		:31;
+	}BITS;
+}__rsa_ktime_t; // RSA_CONSTANT_TIME_REG (0x0820)
+
+typedef union{ // Configures the search option.
+	uint32_t WORD;
+	struct{
+		uint32_t SEARCH_EN	:1;
+		uint32_t			:31;
+	}BITS;
+}__rsa_searchen_t; // RSA_SEARCH_ENABLE_REG (0x0824)
+
+typedef union{ // Configures the starting address to start search
+	uint32_t WORD;
+	struct{
+		uint32_t SEARCH_POS	:12;
+		uint32_t			:20;
+	}BITS;
+}__rsa_searchpos_t; // RSA_SEARCH_POS_REG (0x0828)
+
+typedef union{ // Represents whether or not the RSA memory completes initialization.
+	uint32_t WORD;
+	struct{
+		uint32_t Q_CLEAN	:1;	
+		uint32_t			:31;
+	}BITS;
+}__rsa_queryclean_t; // RSA_QUERY_CLEAN_REG (0x0808)
+
+typedef union{ // Write 1 to clear the RSA interrupt. (WT)
+	uint32_t WORD;
+	struct{
+		uint32_t CLEAR_INT	:1;
+		uint32_t			:31;
+	}BITS;
+}__rsa_intclr_t; // RSA_INT_CLR_REG (0x081C)
+
+typedef union{ // Write 1 to enable the RSA interrupt. (R/W)
+	uint32_t WORD;
+	struct{
+		uint32_t INT_EN	:1;
+		uint32_t		:31;
+	}BITS;
+}__rsa_inten_t; // RSA_INT_ENA_REG (0x082C)
+
+typedef union{ // Write 1 to enable the RSA interrupt. (R/W)
+	uint32_t WORD;
+	struct{
+		uint32_t DATE	:30;
+		uint32_t		:2;
+	}BITS;
+}__rsa_date_t; // RSA_DATE_REG (0x0830)
+
+/*******************************************************************/
+/****************End Bits Fields Type Definition********************/
+/*******************************************************************/
+
+
+/*******************************************************************/
+/****************Start Fields Structure Definition******************/
+/*******************************************************************/
+
+typedef struct{
+	volatile __rsa_mprime_t			M_PRIME;			// Represents M’ 					0x0800 R/W
+	volatile __rsa_mode_t 			MODE;				// Configures RSA length 			0x0804 R/W
+	volatile __rsa_queryclean_t		QUERY_CLEAN;		// RSA initialization status 		0x0808 RO
+	volatile __rsa_modexp_t			SET_START_MODEXP;	// Starts modular exponentiation 	0x080C WT
+	volatile __rsa_modmult_t		SET_START_MODMULT;	// Starts modular multiplication 	0x0810 WT
+	volatile __rsa_mult_t			SET_START_MULT;		// Starts multiplication 			0x0814 WT
+	volatile __rsa_status_t			QUERY_IDLE;			// RSA status 						0x0818 RO
+	volatile __rsa_intclr_t			INT_CLR;			// Clear RSA interrupt 				0x081C WT
+	volatile __rsa_ktime_t			CONSTANT_TIME;		// Configures constant_time option 	0x0820 R/W
+	volatile __rsa_searchen_t		SEARCH_ENABLE;		// Configures search option 		0x0824 R/W
+	volatile __rsa_searchpos_t		SEARCH_POS;			// Configures the search position 	0x0828 R/W
+	volatile __rsa_inten_t			INT_ENA;			// Enable RSA interrupt 			0x082C R/W
+	volatile __rsa_date_t			DATE;				// Version control register 		0x0830 R/W
+} __rsa__t;
+
+/*******************************************************************/
+/****************End Fields Structure Definition********************/
+/*******************************************************************/
+
+/*******************************************************************/
+/****************Start MACROs per Register**************************/
+/*******************************************************************/
+
+#define sRSA (*((__rsa__t *)0x6008A800))
+
+// to RSA_M_PRIME_REG (0x0800)
+#define rRSA_M_PRIME sRSA.M_PRIME.WORD
+
+// to RSA_MODE_REG (0x0804)
+#define rRSA_MODE sRSA.MODE.WORD
+#define bRSA_MODE sRSA.MODE.BITS.MODE
+
+// to RSA_QUERY_CLEAN_REG (0x0808)
+#define rRSA_QUERY_CLEAN sRSA.QUERY_CLEAN.WORD
+#define bRSA_QUERY_CLEAN sRSA.QUERY_CLEAN.BITS.Q_CLEAN
+
+// to RSA_SET_START_MODEXP_REG (0x080C)
+#define rRSA_SET_START_MODEXP sRSA.SET_START_MODEXP.WORD
+#define bRSA_SET_START_MODEXP sRSA.SET_START_MODEXP.BITS.START_MODEXP
+
+// to RSA_SET_START_MODMULT_REG (0x0810)
+#define rRSA_SET_START_MODMULT sRSA.SET_START_MODMULT.WORD
+#define bRSA_SET_START_MODMULT sRSA.SET_START_MODMULT.BITS.START_MODMULT
+
+// to RSA_SET_START_MULT_REG (0x0814)
+#define rRSA_SET_START_MULT sRSA.SET_START_MULT.WORD
+#define bRSA_SET_START_MULT sRSA.SET_START_MULT.BITS.START_MULT
+
+// to RSA_QUERY_IDLE_REG (0x0818)
+#define rRSA_QUERY_IDLE sRSA.QUERY_IDLE.WORD
+#define bRSA_QUERY_IDLE sRSA.QUERY_IDLE.BITS.Q_IDLE
+
+// to RSA_INT_CLR_REG (0x081C)
+#define rRSA_INT_CLR sRSA.INT_CLR.WORD
+#define bRSA_INT_CLR sRSA.INT_CLR.BITS.CLEAR_INT
+
+// to RSA_CONSTANT_TIME_REG (0x0820)
+#define rRSA_CONSTANT_TIME sRSA.CONSTANT_TIME.WORD
+#define bRSA_CONSTANT_TIME sRSA.CONSTANT_TIME.BITS.kTIME
+
+// to RSA_SEARCH_ENABLE_REG (0x0824)
+#define rRSA_SEARCH_ENABLE sRSA.SEARCH_ENABLE.WORD
+#define bRSA_SEARCH_ENABLE sRSA.SEARCH_ENABLE.BITS.SEARCH_EN
+
+// to RSA_SEARCH_POS_REG (0x0828)
+#define rRSA_SEARCH_POS sRSA.SEARCH_POS.WORD
+#define bRSA_SEARCH_POS sRSA.SEARCH_POS.BITS.SEARCH_POS
+
+// to RSA_INT_ENA_REG (0x082C)
+#define rRSA_INT_ENA sRSA.INT_ENA.WORD
+#define bRSA_INT_ENA sRSA.INT_ENA.BITS.INT_EN
+
+// to RSA_DATE_REG (0x0830)
+#define rRSA_DATE sRSA.DATE.WORD
+#define bRSA_DATE sRSA.DATE.BITS.DATE
+
+/*******************************************************************/
+/****************End MACROs per Register****************************/
+/*******************************************************************/
+
+
+#endif /* RSA_LIB_H_ */
